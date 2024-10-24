97582a4c0e1ac767f3a1d61a7c0c5f54f17e5bce
Michael <michael@devereux.fm>
2024-10-24 15:12:55 +0200

    day 3 complete

--- day_3/main.go
+++ day_3/main.go
@@ -14,13 +14,16 @@ type partNumber struct {
 	start int
 	end int
 	lineNumber int
+	toAddRatios bool
 }
 
 
 func main() {
+
 	var slice []string
 	var partNumbers []partNumber
 	total := 0
+	gearRatios := 0
 
 	// LINE_LENGTH := 0
 
@@ -45,9 +48,69 @@ func main() {
 		for i, v := range splitLine {
 			n, err := strconv.Atoi(v)
 			if err == nil {
-				partNumber := partNumber{value: n, start: i + indexOffset, end: i + indexOffset + len(v), lineNumber: lineNumber}
+				partNumber := partNumber{value: n, start: i + indexOffset, end: i + indexOffset + len(v), lineNumber: lineNumber, toAddRatios: false}
 				indexOffset += len(v)
 				partNumbers = append(partNumbers, partNumber)
+			}
+		}
+
+		for i, v := range line {
+			if string(v) == "*" {
+				fmt.Println("===========================")
+				var adjacentNumbers []int
+				// left := i-1
+				// if left < 0 {
+				// 	left = 0
+				// }
+				// right := i+1
+				// if right > LINE_LENGTH {
+				// 	right = LINE_LENGTH
+				// }
+				// top := lineNumber-1
+				// if top < 0 {
+				// 	top = 0
+				// }
+				// bottom := lineNumber +1
+				// if bottom > LINE_COUNT {
+				// 	bottom = LINE_COUNT
+				// }
+
+				if lineNumber > 0 {
+					fmt.Println(slice[lineNumber-1])
+				}
+				fmt.Println(line, i)
+				if lineNumber < LINE_COUNT {
+					fmt.Println(slice[lineNumber+1])
+				}
+				for range i {
+					fmt.Print(" ")
+				}
+				fmt.Print("*")
+				fmt.Println()
+
+				if lineNumber > 0 {
+					adjacentNumbers = append(adjacentNumbers, getNumbersFromLine(slice[lineNumber-1], i)...)
+					fmt.Println(adjacentNumbers, len(adjacentNumbers))
+				}
+				
+				adjacentNumbers = append(adjacentNumbers, getNumbersFromLine(line, i)...)
+				fmt.Println(adjacentNumbers, len(adjacentNumbers))
+				if lineNumber < LINE_COUNT {
+					adjacentNumbers = append(adjacentNumbers, getNumbersFromLine(slice[lineNumber+1], i)...)
+					fmt.Println(adjacentNumbers, len(adjacentNumbers))
+				}
+
+
+				if len(adjacentNumbers) > 0 {
+					if len(adjacentNumbers) > 2 {
+						fmt.Println(adjacentNumbers, "==========================================================================================================")
+					}
+					if len(adjacentNumbers) == 2 {
+						fmt.Println(adjacentNumbers, len(adjacentNumbers), adjacentNumbers[0] * adjacentNumbers[1])
+						gearRatios += adjacentNumbers[0] * adjacentNumbers[1]
+					}
+
+				}
 			}
 		}
 	}
@@ -90,13 +153,13 @@ func main() {
 
 	for _, part := range partNumbers {
 		if part.lineNumber == ln {
-			fmt.Printf("Part Number: %d, Start: %d, End: %d, Line Number: %d\n", part.value, part.start, part.end, part.lineNumber)
+			// fmt.Printf("Part Number: %d, Start: %d, End: %d, Line Number: %d\n", part.value, part.start, part.end, part.lineNumber)
 		}
 	}
 
-	fmt.Println(slice[ln])
-	
 	fmt.Println(total)
+	fmt.Println(gearRatios)
+	// getNumbersFromLine("..........905./50.........@...................971..................762.*..................169........91682..........=......533.......502..", 104)
 
 }
 
@@ -126,4 +189,41 @@ func removeSymbols(str string) string {
 	}
 
 	return newString
+}
+
+func getNumbersFromLine(line string, lineIndex int) (result []int) {
+
+	leftNumber := ""
+	if lineIndex >= 0 && isNumber(string(line[lineIndex-1])) {
+		for i := lineIndex -1; i >= 0 && isNumber(string(line[i])); i-- {
+			leftNumber = string(line[i]) + leftNumber
+		}
+	}
+	rightNumber := ""
+	if lineIndex <= len(line) && isNumber(string(line[lineIndex+1])) {
+		for i := lineIndex + 1; i < len(line) && isNumber(string(line[i])); i++ {
+			rightNumber = rightNumber + string(line[i])
+		}
+	}
+
+	if isNumber(string(line[lineIndex])) {
+		finalNumber := leftNumber + string(line[lineIndex]) + rightNumber
+		finalNumberInt, _ := strconv.Atoi(finalNumber)
+
+		result = append(result, finalNumberInt)
+		fmt.Println("Final", finalNumber)
+	} else {
+		if leftNumber != "" {
+			leftNumberInt, _ := strconv.Atoi(leftNumber)
+			result = append(result, leftNumberInt)
+		}
+		if rightNumber != "" {
+			rightNumberInt, _ := strconv.Atoi(rightNumber)
+			result = append(result, rightNumberInt)
+		}
+		
+		fmt.Println("Final", leftNumber, rightNumber)
+	}
+	return 
+
 }
