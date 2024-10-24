1306ccdfda7be175adb0a04894603c604ebcf193
Michael <michael@devereux.fm>
2024-10-24 15:47:53 +0200

    day 4 started, reads file

--- day_4/main.go
+++ day_4/main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
 	fmt.Println("1")
+	file, err := os.Open("day_4/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+	}
 }
