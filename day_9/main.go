94782caf3508e3ec5d51524d76bf955f5e977d62
Michael <michael@devereux.fm>
2024-10-29 15:26:52 +0100

    day 9 initialized

---
+++ day_9/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, _ := os.Open("day_9/input_test.txt")
+
+	scanner := bufio.NewScanner(file)
+
+	scanner.Scan()
+	fmt.Println(scanner.Text())
+}
